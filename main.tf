0c0c0a6c7915931a91ba77eb41b9daecdfca5504
madhumathi04 <97741195+madhumathi04@users.noreply.github.com>
2023-12-22 21:23:42 +0530

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,87 @@
+# Create S3 bucket
+resource "aws_s3_bucket" "mybucket" {
+  bucket = "myterraformprojectwebsite2001"
+}
+
+# Set ownership controls for the bucket
+resource "aws_s3_bucket_ownership_controls" "example" {
+  bucket = aws_s3_bucket.mybucket.id
+
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
+# Configure public access block for the bucket
+resource "aws_s3_bucket_public_access_block" "example" {
+  bucket = aws_s3_bucket.mybucket.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+# Set public ACL for the bucket
+resource "aws_s3_bucket_acl" "example" {
+  bucket = aws_s3_bucket.mybucket.id
+  acl    = "public-read"
+}
+
+# Upload HTML, CSS, and JavaScript files to the bucket
+resource "aws_s3_object" "index" {
+  bucket = aws_s3_bucket.mybucket.id
+  key    = "index.html"
+  source = "index.html"
+  acl    = "public-read"
+  content_type = "text/html"
+}
+
+resource "aws_s3_object" "script" {
+  bucket = aws_s3_bucket.mybucket.id
+  key    = "script.js"
+  source = "script.js"
+  acl    = "public-read"
+  content_type = "application/javascript"
+}
+
+resource "aws_s3_object" "style" {
+  bucket = aws_s3_bucket.mybucket.id
+  key    = "style.css"
+  source = "style.css"
+  acl    = "public-read"
+  content_type = "text/css"
+}
+
+resource "aws_s3_object" "error" {
+  bucket = aws_s3_bucket.mybucket.id
+  key    = "error.html"
+  source = "error.html"
+  acl    = "public-read"
+  content_type = "text/html"
+}
+
+# Configure S3 bucket website hosting
+resource "aws_s3_bucket_website_configuration" "website" {
+  bucket = aws_s3_bucket.mybucket.id
+
+  index_document {
+    suffix = "index.html"
+  }
+
+  error_document {
+    key = "error.html"
+  }
+
+  routing_rules = <<EOF
+[{
+    "Condition": {
+        "KeyPrefixEquals": "docs/"
+    },
+    "Redirect": {
+        "ReplaceKeyPrefixWith": ""
+    }
+}]
+EOF
+  depends_on = [aws_s3_bucket_acl.example]
+}
